ea8e92200661afe60ac0df1d7f70e0e095cc676f
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-07-08 22:31:08 -0500

    checkpoint components library

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import albumData from './../data/albums';
+import { Link } from 'react-router-dom';
 
-const Library = () => (
-  <section className="library">
-    Library will go here
-  </section>
-);
+class Library extends Component {
+  constructor(props) {
+     super(props);
+      this.state = { albums: albumData };
+   }
+   render() {
+    return (
+      <section className='library'>
+      {
+        this.state.albums.map( (album, index) =>
+          <Link to={`/album/${album.slug}`} key={index}>
+            {album.title}
+          </Link>
+        )
+      }
+      </section>
+     );
+   }
+ }
 
 export default Library;
