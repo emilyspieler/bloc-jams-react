d37e4ab5ff8bc8f5f584162faf682ce3160962a4
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-07-16 20:44:09 -0500

    audio controls checkpoint final

--- src/components/Album.js
+++ src/components/Album.js
@@ -55,12 +55,20 @@ import PlayerBar from './PlayerBar';
      }
 
      handlePrevClick() {
-    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
      const newIndex = Math.max(0, currentIndex - 1);
      const newSong = this.state.album.songs[newIndex];
      this.setSong(newSong);
      this.play();
 }
+
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+    }
 
 
     displayIcons(song, index){
@@ -109,6 +117,7 @@ import PlayerBar from './PlayerBar';
          currentSong={this.state.currentSong}
          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
          handlePrevClick={() => this.handlePrevClick()}
+         handleNextClick={() => this.handleNextClick()}
        />
         </section>
       );
